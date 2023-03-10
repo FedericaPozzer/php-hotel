60662bce3bdce1214d7d6ae1592960f739639577
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-10 16:18:26 +0100

    ok keys (kinda)

--- index.php
+++ index.php
@@ -51,32 +51,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
 
-    <!-- BS -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
 <body>
 
-    <!-- <div>
+    <!-- OK <div>
         <?php foreach($hotels as $hotel) { ?>
-
-        
             <div class="container">
                 <ul>
                     <?php foreach($hotel as $info) { ?>
-
                         <li>
                             <?php echo $info ?>
                         </li>
-                    
                     <?php } ?>
                 </ul>
             </div>
-
-
         <?php } ?>
-
     </div> -->
+
+
 
     <div class="container pt-5">
 
@@ -93,16 +88,24 @@
             </thead>
             <tbody>
                 <tr>
-                    <th> :( </th>
+                    
+                    <?php foreach($hotel as $key => $ciao) { ?>
 
-                    <?php foreach($hotels as $hotel) { ?>
-                        <td> 
-                            <?php echo $hotel["description"] ?>
-                        </td>
-                    <?php } ?>
+
+                    <th>
+                        <?php echo $key ?>
+                    </th>
 
                     
+                    <tr>
+                    <?php foreach($hotels as $hotel) { ?>
 
+
+                    <?php } ?>
+                    </tr>
+                    
+                    
+                    <?php } ?>
 
 
                 </tr>
@@ -112,7 +115,7 @@
     </div>
 
 
-    <!-- BS -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
